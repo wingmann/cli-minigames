fd2f6b75848dd40e911f95378d1c05e5042604aa
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-09-29 18:20:30 +0300

    Improved readability

--- include/cli_minigames/utilities/convert.h
+++ include/cli_minigames/utilities/convert.h
@@ -6,135 +6,91 @@
 #include <optional>
 #include <string>
 
-/// @brief Safe converts string to primitive integral.
-///
-/// @tparam T    The type convert to.
-/// @param f     Pointer to converter function.
-/// @param radix The base of a system of numeration.
-/// @return      Converted integral value.
-///
 template<typename T>
 requires std::integral<T>
 std::optional<T> safe_convert(
     const std::string& value,
-    T (*f)(const std::string&, std::size_t*, int))
+    T (* func)(const std::string&, std::size_t*, int))
 {
     if (value.empty()) {
         std::cerr << "[!] Your input is empty.\n";
         return std::nullopt;
     }
     try {
-        return f(value, nullptr, 10);
-    } catch (std::out_of_range&) {
-        std::cerr << "[!] Too long a number.\n";
+        return func(value, nullptr, 10);
     } catch (std::invalid_argument&) {
+        std::cerr << "[!] Too long a number.\n";
+    } catch (std::out_of_range&) {
         std::cerr << "[!] Your input is incorrect.\n";
     }
     return std::nullopt;
 }
 
-/// @brief Safe converts string to primitive floating point.
-///
-/// @tparam T The type convert to.
-/// @param f  Pointer to converter function.
-/// @return   Converted integral value.
-///
 template<typename T>
 requires std::floating_point<T>
-std::optional<T> safe_convert(const std::string& value, T (*f)(const std::string&, std::size_t*)) {
+std::optional<T> safe_convert(
+    const std::string& value,
+    T (* func)(const std::string&, std::size_t*))
+{
     if (value.empty()) {
         std::cerr << "[!] Your input is empty.\n";
         return std::nullopt;
     }
     try {
-        return f(value, nullptr);
-    } catch (std::out_of_range&) {
-        std::cerr << "[!] Your input is incorrect.\n";
+        return func(value, nullptr);
     } catch (std::invalid_argument&) {
+        std::cerr << "[!] Your input is incorrect.\n";
+    } catch (std::out_of_range&) {
         std::cerr << "[!] Too long a number.\n";
     }
     return std::nullopt;
 }
 
-/// @brief Converts string to primitive integral value.
-///
-/// @tparam T Type of integral value to which to convert.
-/// @return   If converted, then integral value otherwise std::nullopt
-///
 template<std::integral T>
 std::optional<T> to_integer(const std::string& value, int radix = 10);
 
-/// @brief Converts string to primitive floating point value.
-///
-/// @tparam T Type of floating point value to which to convert.
-/// @return   If converted, then floating point value otherwise std::nullopt
-///
 template<std::floating_point T>
 std::optional<T> to_floating(const std::string& value);
 
-/// @brief Converts string to primitive integral value.
-///
-/// @tparam T Type of integral value to which to convert.
-/// @return   If converted, then integral value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<int> to_integer<int>(const std::string& value, int radix) {
+std::optional<int> to_integer<int>(const std::string& value, int radix)
+{
     return safe_convert(value, std::stoi);
 }
 
-/// @brief Converts string to primitive integral value.
-///
-/// @tparam T Type of integral value to which to convert.
-/// @return   If converted, then integral value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<std::int64_t> to_integer<std::int64_t>(const std::string& value, int radix) {
+std::optional<std::int64_t> to_integer<std::int64_t>(const std::string& value, int radix)
+{
     return safe_convert(value, std::stoll);
 }
 
-/// @brief Converts string to primitive integral value.
-///
-/// @tparam T Type of integral value to which to convert.
-/// @return   If converted, then integral value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<std::uint64_t> to_integer<std::uint64_t>(const std::string& value, int radix) {
+std::optional<std::uint64_t> to_integer<std::uint64_t>(const std::string& value, int radix)
+{
     return safe_convert(value, std::stoull);
 }
 
-/// @brief Converts string to primitive floating point value.
-///
-/// @tparam T Type of floating point value to which to convert.
-/// @return   If converted, then floating point value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<float> to_floating<float>(const std::string& value) {
+std::optional<float> to_floating<float>(const std::string& value)
+{
     return safe_convert(value, std::stof);
 }
 
-/// @brief Converts string to primitive floating point value.
-///
-/// @tparam T Type of floating point value to which to convert.
-/// @return   If converted, then floating point value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<double> to_floating<double>(const std::string& value) {
+std::optional<double> to_floating<double>(const std::string& value)
+{
     return safe_convert(value, std::stod);
 }
 
-/// @brief Converts string to primitive floating point value.
-///
-/// @tparam T Type of floating point value to which to convert.
-/// @return   If converted, then floating point value otherwise std::nullopt
-///
 template<>
 [[nodiscard]]
-std::optional<long double> to_floating<long double>(const std::string& value) {
+std::optional<long double> to_floating<long double>(const std::string& value)
+{
     return safe_convert(value, std::stold);
 }
 
--- src/guessing_game/main.cpp
+++ src/guessing_game/main.cpp
@@ -1,6 +1,7 @@
 #include "cli_minigames/utilities.h"
 
-int main() {
+int main()
+{
     std::cout << "Guess the number!\n";
 
     auto secret_number = generate_number(1, 100);
@@ -8,9 +9,10 @@ int main() {
 
     while (true) {
         auto guess = read_line("Please input your guess");
-
         auto number = to_integer<int>(guess);
-        if (!number.has_value()) continue;
+
+        if (!number.has_value())
+            continue;
 
         if (number < 1 || number > 100) {
             std::cerr << "[!] Secret number ranges from 1 to 100\n";
@@ -20,9 +22,11 @@ int main() {
 
         if (number < secret_number) {
             std::cout << "- too small!\n";
-        } else if (number > secret_number) {
+        }
+        else if (number > secret_number) {
             std::cout << "- to big!\n";
-        } else {
+        }
+        else {
             std::cout << "You win!\nCount of attempts: " << counter << '\n';
             break;
         }
